90ffa7e4494b1d28d9b766fc6e6d05135a8a295c
Machiel <maddogbroekman@gmail.com>
2026-07-16 11:06:20 +0200

    chore: bump package version to 0.1.0b18.1

--- industry_reforged/__init__.py
+++ industry_reforged/__init__.py
@@ -1,6 +1,6 @@
 """Initialize the app"""
 
-__version__ = "0.1.0b18"
+__version__ = "0.1.0b18.1"
 __title__ = "Industry"
 __author__ = "Maddog Broekman"
 __copyright__ = "Copyright 2026 Maddog Broekman"
