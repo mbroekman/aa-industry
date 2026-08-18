84c3c9f3c057d86450879f8d34765bbb1932e87b
Machiel <maddogbroekman@gmail.com>
2026-08-18 12:33:28 +0200

    chore: remove junk test files and shell scripts from root directory

--- run_compile_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run --rm -v $(pwd):/app -w /app python:3.12-slim bash -c "
-apt-get update && apt-get install -y gettext
-pip install django
-django-admin compilemessages -l nl -i .tox -i .venv
-"
--- run_tests_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run --rm -v $(pwd):/app -w /app python:3.13-rc-slim bash -c "
-apt-get update && apt-get install -y default-libmysqlclient-dev pkg-config gcc redis-server
-service redis-server start
-pip install tox
-tox -e py313
-"
